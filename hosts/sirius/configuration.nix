870c9b981834d5cc5fad308e74e86f9a6bf0c492
Luís Fonseca <luis@lhf.pt>
2021-07-19 18:17:21 +0100

    Configure Fonts
    monospace: Nerd Fonts Patched IBM Plex Mono + Noto Sans Mono CJK
    sans: IBM Plex Sans + Noto Sans CJK
    serif: IBM Plex Serif + Noto Sans CJK
    
    All Noto Sans should be removed once IBM Plex CJK is released.

--- hosts/sirius/configuration.nix
+++ hosts/sirius/configuration.nix
@@ -62,6 +62,23 @@
     extraGroups = [ "wheel" ];
   };
 
+  fonts = {
+    enableDefaultFonts = true;
+    fonts = with pkgs; [
+      (nerdfonts.override { fonts = [ "IBMPlexMono" ]; })
+      ibm-plex
+      noto-fonts-cjk
+      noto-fonts-emoji
+    ];
+
+    fontconfig.defaultFonts = {
+      serif = [ "IBM Plex Serif" "Noto Sans CJK JP" ];
+      sansSerif = [ "IBM Plex Sans" "Noto Sans CJK JP" ];
+      monospace = [ "BlexMono Nerd Font Mono" "Noto Sans Mono CJK JP" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
+
   environment = {
     systemPackages = with pkgs; [
       neovim
