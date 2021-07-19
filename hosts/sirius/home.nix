7260ecad977956d42a773b89852fc4b3f0a1b0d4
Luís Fonseca <luis@lhf.pt>
2021-07-19 18:20:37 +0100

    Preserve my sanity

--- hosts/sirius/home.nix
+++ hosts/sirius/home.nix
@@ -47,4 +47,8 @@
   home.file."${config.programs.gpg.homedir}/sshcontrol".text = ''
     B155DE05293E0A22B220AB1F8D3414A3E7DED3CF
   '';
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
 }
