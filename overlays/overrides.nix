7ae940f736ac435729355858a17dd307d17abaea
Luís Fonseca <luis@lhf.pt>
2021-08-13 19:45:41 +0100

    unstable is now the default

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -6,7 +6,5 @@
 # Overrides nixpkgs. Useful from getting a pkg from latest.
 
 self: super: {
-  inherit (self.unstable) neovim;
-  inherit (self.unstable) xkeyboard_config; # Version 2.33 is needed for colemak_dh
   inherit (self.latest) discord; # Will soft lock trying to get latest version
 }
