84c02b50d7ff1497a28e8cb7dc636c7dffb1ddcc
Luís Fonseca <luis@lhf.pt>
2021-09-10 16:31:02 +0100

    move bspwm config to module

---
+++ modules/home/bspwm.nix
@@ -0,0 +1,36 @@
+# modules/home/bspwm.nix
+#
+# Author: Luís Fonseca <luis@lhf.pt>
+# URL:    https://github.com/luishfonseca/dotfiles
+#
+# BSPWM home configuration.
+
+{ ... }:
+
+{
+  xsession = {
+    enable = true;
+    windowManager.bspwm.enable = true;
+  };
+  
+  services.sxhkd.keybindings = {
+
+    # general
+
+    "super + shift + {q,r}" = "bspc {quit,wm -r}"; # quit/restart bspwm
+    "super + {_,shift + }c" = "bspc node -{c,k}"; # close/kill node
+    "super + m"             = "bspc desktop -l next"; # toggle monocle layout
+    "super + g"             = "bspc node -s biggest.window"; # swap with biggest
+
+    # focus/swap
+
+    "super + {_,shift + }{h,j,k,l}" = "bspc node -{f,s} {west,south,north,east}"; # direction
+    "super + {p,b,comma,period}"    = "bspc node -f @{parent,brother,first,second}"; # path
+
+    # resize
+
+    "super + alt + {h,j,k,l}"         = "bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}"; # expand
+    "super + alt + shift + {h,j,k,l}" = "bspc node -z {right -20 0,top 0 20,bottom 0 -20,left 20 0}"; # contract
+
+  };
+}
