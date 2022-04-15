2ce4f625684ab05654d49d5313e5f225bbf4e087
Luís Fonseca <luis@lhf.pt>
2022-04-15 22:52:00 +0100

    comment out unconfigured systems

--- hosts.nix
+++ hosts.nix
@@ -1,13 +1,13 @@
 {
-  altair = {
-    system = "x86_64-linux";
-    gpg.sshKeys = "DB4E50598EBC42BC801BBEC6C39A9A399FD27081";
-  };
+  # altair = {
+  #   system = "x86_64-linux";
+  #   gpg.sshKeys = "DB4E50598EBC42BC801BBEC6C39A9A399FD27081";
+  # };
 
-  sirius = {
-    system = "x86_64-linux";
-    gpg.sshKeygrip = "B155DE05293E0A22B220AB1F8D3414A3E7DED3CF";
-  };
+  # sirius = {
+  #   system = "x86_64-linux";
+  #   gpg.sshKeygrip = "B155DE05293E0A22B220AB1F8D3414A3E7DED3CF";
+  # };
 
   procyon = {
     system = "x86_64-linux";
