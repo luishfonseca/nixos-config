bf88d389dee3c29ff886179fe25464eaa521410b
Luís Fonseca <luis@lhf.pt>
2022-04-18 18:51:09 +0100

    common: fonts

---
+++ modules/common.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+{
+  fonts = {
+    fonts = with pkgs;
+      [
+        (nerdfonts.override { fonts = [ "IBMPlexMono" ]; })
+        ibm-plex
+        noto-fonts-emoji
+      ];
+
+    fontconfig.defaultFonts = {
+      serif = [ "IBM Plex Serif" "IBM Plex Sans JP" ];
+      sansSerif = [ "IBM Plex Sans" "IBM Plex Sans JP" ];
+      monospace = [ "BlexMono Nerd Font Mono" "IBM Plex Sans JP" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
+}
