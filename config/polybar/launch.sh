a953a7342cbcee61ba05cd832396ef88fb0652f8
Luís Fonseca <luis@lhf.pt>
2022-07-14 20:31:41 +0100

    polybar: show on all monitors

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 polybar-msg cmd quit
-polybar --config=$XDG_CONFIG_HOME/polybar/config.ini example &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --config=$XDG_CONFIG_HOME/polybar/config.ini example &
+  done
+else
+  polybar --config=$XDG_CONFIG_HOME/polybar/config.ini example &
+fi
