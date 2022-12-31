0bbf6a88d63837b5ab10460e2f1c0728a209a6ab
Luís Fonseca <luis@lhf.pt>
2022-12-31 14:05:43 +0000

    remove hardcoded values from git module

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -3,21 +3,45 @@
 with lib;
 let cfg = config.lhf.programs.git; in
 {
-  options.lhf.programs.git.enable = mkEnableOption "Git";
-
-  config.programs.git = mkIf cfg.enable {
-    enable = true;
-    config = {
-      init.defaultBranch = "main";
-      commit.gpgSign = true;
-      gpg.format = "ssh";
-      url."ssh://git@github.com/".insteadOf = [ "https://github.com/" ];
-      user = {
-        #TODO: these should not be hardcoded
-        name = "Luís Fonseca";
-        email = "luis@lhf.pt";
-        signingkey = config.lhf.services.ssh.user.key;
+  options.lhf.programs.git = {
+    enable = mkEnableOption "Git";
+    commits = {
+      name = mkOption {
+        type = types.str;
+        default = "Luís Fonseca";
+        description = "Name to use in commits";
+      };
+      email = mkOption {
+        type = types.str;
+        default = "luis@lhf.pt";
+        description = "Email to use in commits";
+      };
+      signingkey = mkOption {
+        type = types.nullOr types.str;
+        default = if config.lhf.services.ssh.enable then config.lhf.services.ssh.user.key else null;
+        description = "SSH key to use for signing commits";
       };
     };
   };
+
+  config.programs.git = mkIf cfg.enable (mkMerge [
+    {
+      enable = true;
+      config = {
+        init.defaultBranch = "main";
+        url."ssh://git@github.com/".insteadOf = [ "https://github.com/" ];
+        user = {
+          name = cfg.commits.name;
+          email = cfg.commits.email;
+        };
+      };
+    }
+    (mkIf (cfg.commits.signingkey != null) {
+      config = {
+        commit.gpgSign = true;
+        gpg.format = "ssh";
+        user.signingkey = cfg.commits.signingkey;
+      };
+    })
+  ]);
 }
