35da40b518c8f4cff1c74751e30a8deb6877efde
Luís Fonseca <luis@lhf.pt>
2023-07-05 13:34:14 +0100

    feat: add config flake to registry

--- modules/nixconf.nix
+++ modules/nixconf.nix
@@ -3,7 +3,10 @@
 {
   environment.variables.NIXPKGS_ALLOW_UNFREE = "1";
   nix = {
-    registry.nixpkgs.flake = inputs.nixpkgs;
+    registry = {
+      nixpkgs.flake = inputs.nixpkgs;
+      config.flake = inputs.self;
+    };
     nixPath = [
       "nixpkgs=${inputs.nixpkgs}"
     ];
