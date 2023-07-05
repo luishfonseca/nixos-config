ce33e2ab82bb59957468a69508c8008da4d3c5df
Luís Fonseca <luis@lhf.pt>
2023-07-05 13:48:52 +0100

    feat: use nvd to diff stores on activation

--- modules/upgrade-diff.nix
+++ modules/upgrade-diff.nix
@@ -1,8 +1,5 @@
-# By Mic92:
-# https://github.com/Mic92/dotfiles/blob/c83bcecc32bfd9bc96ba97f518a7e4ccd63393ec/nixos/modules/upgrade-diff.nix
-
 { pkgs, ... }: {
   system.activationScripts.diff = ''
-    ${pkgs.nix}/bin/nix store diff-closures /run/current-system "$systemConfig"
+    ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff /run/current-system "$systemConfig"
   '';
 }
