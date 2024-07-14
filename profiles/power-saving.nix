ea91ff8efa3ccbebcddf76e0983a2a264db6a0a0
Luís Fonseca <luis@lhf.pt>
2024-07-14 01:21:26 +0100

    feat: add more power saving services

--- profiles/power-saving.nix
+++ profiles/power-saving.nix
@@ -1,4 +1,8 @@
 {
   powerManagement.powertop.enable = true;
-  services.tlp.enable = true;
+  services = {
+    tlp.enable = true;
+    thermald.enable = true;
+    auto-cpufreq.enable = true;
+  };
 }
