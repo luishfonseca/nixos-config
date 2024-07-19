2793368e903aa3a65903797eb05552a5acc56c49
Luís Fonseca <luis@lhf.pt>
2024-07-19 23:41:04 +0100

    feat: deploy sirius

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,14 @@
 let
   pub = import ../public-keys.nix;
 
-  altair = with pub; [user.altair];
   arcturus = with pub; [host.arcturus user.arcturus];
+  sirius = with pub; [host.sirius user.sirius];
 
   deployers = [pub.user.altair];
 in {
   "host-keys/arcturus.age".publicKeys = deployers;
+  "host-keys/sirius.age".publicKeys = deployers;
 
   "arcturus/id_ed25519.age".publicKeys = deployers ++ arcturus;
+  "sirius/id_ed25519.age".publicKeys = deployers ++ sirius;
 }
