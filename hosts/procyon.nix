65f383b35fa60b449c1fae955f8a2ff638c07541
Luís Fonseca <luis@lhf.pt>
2025-11-27 22:40:52 +0100

    fix: keyboard dead after resume

--- hosts/procyon.nix
+++ hosts/procyon.nix
@@ -27,7 +27,13 @@
     ];
   };
 
-  boot.consoleLogLevel = 3;
+  boot = {
+    consoleLogLevel = 3;
+
+    # keyboard dead after suspend fix:
+    # https://gitlab.com/tuxedocomputers/development/packages/linux/-/commit/ac7f9947f4289a476a21eb67e07cdb9669258567
+    kernelParams = ["i8042.nomux=1" "i8042.reset=1" "i8042.noloop=1" "i8042.nopnp=1"];
+  };
 
   nixpkgs.hostPlatform = "x86_64-linux";
   system.stateVersion = "25.05";
