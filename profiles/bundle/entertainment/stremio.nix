df5d2bc1374de3a903593e75c2232e540ee9c3b5
Luís Fonseca <luis@lhf.pt>
2025-12-03 20:16:14 +0100

    feat: use mpv with stremio

--- profiles/bundle/entertainment/stremio.nix
+++ profiles/bundle/entertainment/stremio.nix
@@ -1,5 +1,11 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     lhf.stremio-service
+    mpv
   ];
+
+  hm.xdg.mimeApps = {
+    enable = true;
+    defaultApplications."audio/x-mpegurl" = "mpv.desktop";
+  };
 }
