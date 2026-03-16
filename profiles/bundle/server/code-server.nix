e0cf1762d53971bc1235f01a79d6094096cc614b
Luís Fonseca <luis@lhf.pt>
2026-03-16 17:23:06 +0100

    chore: cleanups

--- profiles/bundle/server/code-server.nix
+++
@@ -1,8 +0,0 @@
-{inputs, ...}: {
-  imports = [inputs.vscode-server.nixosModules.default];
-
-  services.vscode-server.enable = true;
-  systemd.user.services.auto-fix-vscode-server.wantedBy = ["multi-user.target"];
-
-  persist.user.local.directories = [".vscode-server"];
-}
