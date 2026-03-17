4ba4d574a8a76cb31cf31bd2b96f44b060a57eab
Luís Fonseca <luis@lhf.pt>
2026-03-17 14:57:50 +0100

    feat: add remote-rebuild script

--- flake.nix
+++ flake.nix
@@ -202,6 +202,10 @@
               sops
               lhf.prepare-secrets
               lhf.deploy-anywhere
+
+              (writeShellScriptBin "remote-rebuild" ''
+                exec nixos-rebuild $2 --flake .#$1 --build-host $1 --target-host $1 --ask-sudo-password --no-reexec
+              '')
             ]
             ++ checks.${system}.pre-commit-check.enabledPackages;
         };
