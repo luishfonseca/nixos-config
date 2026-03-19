b72441c5dea6632f0e42afaea99027d5d8678b46
Luís Fonseca <luis@lhf.pt>
2026-03-19 03:23:19 +0100

    feat: don't autologin all machines

--- profiles/bundle/common/tailscale.nix
+++ profiles/bundle/common/tailscale.nix
@@ -2,17 +2,20 @@
   config,
   lib,
   ...
-}: let
-  hasTailscaleAuthKey = builtins.hasAttr "tailscale-auth-key" config.sops.secrets;
-in {
-  services.tailscale = {
-    enable = true;
-    openFirewall = true;
-    authKeyFile = lib.mkIf hasTailscaleAuthKey config.sops.secrets.tailscale-auth-key.path;
-    authKeyParameters = {
-      ephemeral = false;
-      preauthorized = true;
-    };
-    extraUpFlags = ["--advertise-tags=tag:fleet,tag:${config.networking.hostName}"];
-  };
+}: {
+  services.tailscale = lib.mkMerge [
+    {
+      enable = true;
+      openFirewall = true;
+      extraSetFlags = ["--operator=${config.user.name}"];
+    }
+    (lib.mkIf (builtins.hasAttr "tailscale-auth-key" config.sops.secrets) {
+      authKeyFile = config.sops.secrets.tailscale-auth-key.path;
+      authKeyParameters = {
+        ephemeral = false;
+        preauthorized = true;
+      };
+      extraUpFlags = ["--advertise-tags=tag:fleet,tag:${config.networking.hostName}"];
+    })
+  ];
 }
