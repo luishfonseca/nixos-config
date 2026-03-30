b10508d1e0d1046eee57d295e0717510e3ddaff2
Luís Fonseca <luis@lhf.pt>
2026-03-31 01:21:29 +0200

    feat: setup encrypted vault in storage box

--- profiles/storage-box.nix
+++ profiles/storage-box.nix
@@ -23,4 +23,32 @@
       "credentials=${config.sops.secrets.storage-box-credentials.path}"
     ];
   };
+
+  programs.fuse.userAllowOther = true;
+
+  systemd.services.vault = let
+    passfile = config.sops.secrets.gocryptfs-password.path;
+    cipher = "/mnt/box/vault";
+    plain = "/mnt/vault";
+  in {
+    description = "gocryptfs vault on storage box";
+    requires = ["mnt-box.automount"];
+    after = ["mnt-box.automount"];
+    wantedBy = ["multi-user.target"];
+    path = [pkgs.gocryptfs pkgs.util-linux];
+
+    script = ''
+      mkdir -p ${cipher} ${plain}
+      if [ ! -f ${cipher}/gocryptfs.conf ]; then
+        gocryptfs -init -passfile ${passfile} ${cipher}
+      fi
+      exec gocryptfs -fg -allow_other -passfile ${passfile} ${cipher} ${plain}
+    '';
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStop = "${pkgs.util-linux}/bin/fusermount -u ${plain}";
+      Restart = "on-failure";
+    };
+  };
 }
