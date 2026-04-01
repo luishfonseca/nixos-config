72354a76c8c980636f5f4ec98192464cbe7a2486
Luís Fonseca <luis@lhf.pt>
2026-04-01 14:45:17 +0200

    feat: move rmfakecloud to s3

--- profiles/services/rmfakecloud.nix
+++ profiles/services/rmfakecloud.nix
@@ -1,28 +1,67 @@
-{config, ...}: let
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
   port = 3123;
   name = "rmfakecloud";
   url = "https://${name}.${config.lhf.tailscale.tailnet}";
+
+  package = let
+    rev = "4f21ea9860f849057d621eefdafeb65e50a632c9";
+    src = pkgs.fetchFromGitHub {
+      inherit rev;
+      owner = "nemunaire";
+      repo = "rmfakecloud";
+      hash = "sha256-LZrJrmL8LqZVjgVTSO//uQBeGkNYUEPZZ2DMRih9OTQ=";
+    };
+  in
+    pkgs.rmfakecloud.overrideAttrs (old: rec {
+      inherit src;
+      version = rev;
+      vendorHash = "sha256-yCDsi7ExNXIw587StCAnXPM9FoLVQJmCehEOT8phL0k=";
+      env =
+        old.env
+        // {
+          pnpmDeps = pkgs.fetchPnpmDeps {
+            inherit (old) pname;
+            inherit src version;
+            sourceRoot = "${src.name}/ui";
+            pnpmLock = "${src}/ui/pnpm-lock.yaml";
+            inherit (old.env.pnpmDeps) pnpm fetcherVersion;
+            hash = "sha256-SqEcvW6k3JjJHI4nhIt0KCRuWv096yY7CqyOgAzuqbI=";
+          };
+        };
+    });
 in {
   imports = [
     ./caddy-tailscale.nix
   ];
 
   systemd.services.rmfakecloud = {
-    requires = ["vault.service"];
-    after = ["vault.service"];
-    serviceConfig = {
-      SupplementaryGroups = ["box"];
-      BindPaths = ["/mnt/vault/${name}"];
-      ReadWritePaths = ["/mnt/vault/${name}"];
-      MountFlags = "shared";
-    };
+    requires = ["garage.service"];
+    after = ["garage.service"];
   };
 
   services = {
     rmfakecloud = {
+      inherit package port;
       enable = true;
-      inherit port;
-      extraSettings.DATADIR = "/mnt/vault/${name}";
+      extraSettings = {
+        RM_HTTPS_COOKIE = "true";
+        
+        # sudo garage key create rmfakecloud
+        # sudo garage bucket create rmfakecloud
+        # sudo garage bucket allow --read --write --owner rmfakecloud --key rmfakecloud
+        # sudo garage bucket allow --read rmfakecloud --key admin
+        AWS_ENDPOINT_URL = "https://s3.${config.lhf.tailscale.tailnet}";
+        AWS_REGION = "garage";
+        S3_PATH_STYLE = "true";
+        S3_BUCKET_NAME = "rmfakecloud";
+        AWS_ACCESS_KEY_ID = "GK7c6f8c1e0f0a9f30d08a0a01";
+      };
+      environmentFile = config.sops.secrets.rmfakecloud-env.path;
       storageUrl = "https://local.appspot.com";
     };
     caddy.virtualHosts = {
